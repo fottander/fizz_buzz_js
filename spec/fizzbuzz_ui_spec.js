f33dfbb772e7c789bcf0a2e527e2a07bd6a29778
felixottander <felix.ottander@yahoo.com>
2017-02-20 08:36:12 +0100

    Created the ui specs and the local server set up.

--- spec/fizzbuzz_ui_spec.js
+++ spec/fizzbuzz_ui_spec.js
@@ -1,8 +1,14 @@
-describe('BMI_UI - index.html', function() {
+describe('FiZZ-UI - index.html', function() {
     beforeEach(function() {
         jasmine.getFixtures().fixturesPath = '.';
         loadFixtures('index.html');
         $.holdReady(false);
+        $('#input').val('45');
+        $('#play').trigger('click');
+    });
+
+    it("displays fizzbuzz", function() {
+        expect($('#display_message').text()).toBe('fizzbuzz');
     });
 
 });
